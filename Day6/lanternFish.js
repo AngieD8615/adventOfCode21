4f555e23b9d03b8bdaf4ccb09a165afe92309fde
AngieD8615 <adavidson1@gmail.com>
2021-12-18 23:59:22 -0700

    used recursion and memoize to cal exponential grow of fish

--- Day6/lanternFish.js
+++ Day6/lanternFish.js
@@ -49,3 +49,127 @@ After  4 days: 6,0,6,4,5,6,7,8,8
 After  5 days: 5,6,5,3,4,5,6,7,7,8
 
  */
+import { readFileSync } from 'fs'
+
+var memo = (totalDays) => {
+  let memoCount = {} // {initialState: {daysLeft: count}}
+  
+  for (var days = 1; days <= totalDays; days++) {  
+    if (days % 20 === 0) {
+      console.log(`day: ${days}`)
+    }
+    for (var initialAge = 1; initialAge <=6; initialAge++) {
+      var count = countAfterDaysMemo(initialAge, days, memoCount)
+      memoCount[initialAge][days] = count
+    }
+  }
+  return memoCount
+}
+
+var countAfterDaysMemo = (initialAge, days, memoCount) => {
+  let count = 0
+
+  var helper = (curAge, days) => {
+    if (!memoCount[curAge]) {
+      memoCount[curAge] = {}
+    }
+    if (memoCount[curAge][days] != undefined) {
+      count += memoCount[curAge][days] 
+      return 
+    }
+    if (days === 0) {
+      count++
+      return
+    } 
+    if (curAge === 0) {
+      helper (6, days - 1)
+      helper (8, days - 1)
+    } if (curAge > 0) {
+      helper(curAge - 1, days - 1)
+    }
+  }
+  
+  helper(initialAge, days)
+
+  return count
+}
+
+var afterNDays = (initialAge, days) => {
+  let count = 0
+
+  var helper = (curAge, daysLeft) => {
+    if (daysLeft === 0) {
+      count++
+      return
+    } 
+    if (curAge === 0) {
+      helper (6, daysLeft - 1)
+      helper (8, daysLeft - 1)
+    } if (curAge > 0) {
+      helper(curAge - 1, daysLeft - 1)
+    }
+  }
+  
+  helper(initialAge, days)
+
+  return count
+}
+
+
+var parseAndCountInput = (filePath) => {
+  var countInitialState = {}
+  var data = readFileSync(filePath, 'utf8')
+  data = data.split(",")
+  data.forEach(initialState => {
+    if (!countInitialState[initialState]) {
+      countInitialState[initialState] = 1
+    } else {
+      countInitialState[initialState]++
+    }
+  })
+
+  return countInitialState
+}
+
+var calcNUmOfFishTotal = (filePath) => {
+  var totalCount = 0
+  var countInitialState = parseAndCountInput(filePath)
+  var initailStates = Object.keys(countInitialState)
+
+  initailStates.forEach(initState => {
+    var initStateInt = parseInt(initState)
+    var count = afterNDays(initStateInt,  80)
+    totalCount += (count * countInitialState[initState])
+  })
+  return totalCount
+}
+
+var calcNUmOfFishTotalB = (filePath) => {
+  var days = 256
+  var totalCount = 0
+  var countOfInitialState = parseAndCountInput(filePath)
+  var initailStates = Object.keys(countOfInitialState)
+  var numOfDecendants = memo(days) 
+
+  initailStates.forEach(initState => {
+    totalCount += (numOfDecendants[initState][days] * countOfInitialState[initState])
+  })
+  return totalCount
+}
+
+// console.log(`init: 1, days: 99, count: ${afterNDays(1, 99)}, ${afterNDays(2, 99)}, ${afterNDays(3, 99)}, ${afterNDays(4, 99)}, ${afterNDays(5, 99)}`)
+console.log(calcNUmOfFishTotalB('./input.txt'))
+// console.log(calcNUmOfFishTotal('./input.txt'))
+
+// Init  -> 1
+//  0 -> 1
+//  1 -> 0 
+//  2 -> 6 8
+//  3 -> 5 7
+//  4 -> 4 6
+//  5 -> 3 5
+//  6 -> 2 4
+//  7 -> 1 3
+//  8 -> 0 2 
+//  9 -> 6 1 8
+// 10 -> 5 0 7 
